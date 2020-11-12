241747936032fb40579e6fd6d34a59ba54a9089e
mehdisellami <mehdisellami9@gmail.com>
2020-11-12 03:18:15 +0100

    v3.0.3

--- Helpme-Frontend/src/app/components/map/map.component.ts
+++ Helpme-Frontend/src/app/components/map/map.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, MissingTranslationStrategy, OnInit } from '@angular/core';
 import { MissionService } from 'src/app/service/mission.service';
+
+import * as mapboxgl from 'mapbox-gl'
+import { Mission } from '../creation-mission/mission.model';
+import { AppComponent } from 'src/app/app.component';
+
+
+
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
-  
-  public mission:any=[];
+export class MapComponent implements OnInit  {
 
 
 
+
+
+
+  map:mapboxgl.Map;
+  public mission :any= [];
+  public longlat:any=[];
+
+
+  lat:any=2.331685;
+  long:any=48.8942747;
+
+  m:any;
 
   CheckedBx=false;
 
@@ -19,23 +36,68 @@ export class MapComponent implements OnInit {
     this.CheckedBx=true;
  }
 
-  constructor(public RestMission : MissionService) { }
+
+ 
+  
+
+  constructor(public RestMission : MissionService ) {
+   }
 
   ngOnInit(): void {
+
     this.getMission();
+  
+    this.AdresstoLongLAt(this.m);
+    
+  
   }
 
+  
 
+  getMission() {
 
-  getMission(){
-    this.RestMission.GetMission().subscribe(
+   this.RestMission.GetMission().subscribe(
+
       (data )=>{
+
         this.mission=data;
-
-        console.log(data);
-
+        
+          console.log(data);
+         
         },
 
     );
+   
   }
+
+
+  
+AdresstoLongLAt(m){
+
+ 
+  this.RestMission.getLongLat(m).subscribe(
+    data =>{
+
+      this.longlat=data;
+
+      this.longlat= Array.of(this.longlat);
+      console.log(data);
+      
+
+      },
+
+  );
+
+
 }
+
+
+
+
+
+
+  
+}
+
+
+
--- Helpme-Frontend/src/app/service/mission.service.ts
+++ Helpme-Frontend/src/app/service/mission.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Mission } from '../components/creation-mission/mission.model';
 
 
 
-
-
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'any'}
 )
@@ -14,6 +14,8 @@ export class MissionService {
   public urlpostmission="http://localhost:5001/newmission";
 
   public urlgetmission="http://localhost:5001/listeMission";
+
+  public longlat:any=[]; 
 
   constructor(private http: HttpClient) { }
 
@@ -31,4 +33,20 @@ export class MissionService {
 
   }
 
-}
+  getLongLat(adr) {
+    
+    return this.http.get ("https://api.mapbox.com/geocoding/v5/mapbox.places/"+adr+".json?types=address&access_token=pk.eyJ1IjoibWVoZGlzZWxsYW1pIiwiYSI6ImNrZ2t5bnQ3cTA2NDAyeW8xMzBoanVnMm8ifQ.UuXfzm__C3HRsGXfAktk3w")
+    ;
+    
+    
+    
+    }
+
+
+
+
+  }
+
+
+
+
