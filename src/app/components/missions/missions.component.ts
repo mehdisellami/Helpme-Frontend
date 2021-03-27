d4dafdc4a5a96157b2f3082b587349edfc513c7b
rokhaya <baadja43@gmail.com>
2021-03-27 16:16:21 +0100

    tranche age mission

--- src/app/components/missions/missions.component.ts
+++ src/app/components/missions/missions.component.ts
@@ -11,7 +11,7 @@ import { RestService } from '../../service/rest.service';
 export class MissionsComponent implements OnInit {
   public mission:any=[];
   userInstance: User = new User();
-
+  tranche:string="jeune";
   constructor(public missionservice :MissionService , public restservice : RestService) { }
 
   ngOnInit(): void {
@@ -31,34 +31,34 @@ export class MissionsComponent implements OnInit {
     this.missionservice.DeleteMission(id)
     .subscribe( data => {
       alert("Mission " + id + " supprimée");
-      this.reloadPage(); 
+      this.reloadPage();
     },
     (err)=>{alert("ERROR");}
-    );  
+    );
   }
 
   editmission(id):void{
-      window.open("/app-editmission/"+id,"_self");   
+      window.open("/app-editmission/"+id,"_self");
   }
 
   missionterminer(id):void{
     this.missionservice.Missionterminer(id)
     .subscribe( data => {
       alert("Mission " + id + " terminée");
-      this.reloadPage(); 
+      this.reloadPage();
     },
     (err)=>{alert("ERROR");}
-    );  
+    );
   }
 
   annulerfin(id):void{
     this.missionservice.Annulerfin(id)
     .subscribe( data => {
       alert("Vous avez annulé la fin de la mission " + id);
-      this.reloadPage(); 
+      this.reloadPage();
     },
     (err)=>{alert("ERROR");}
-    );  
+    );
   }
 
   reloadPage() {
